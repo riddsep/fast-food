02b539d941e11354da8c2556a8d6165ab2fa6375
riddsep <ridoseptiawan00@gmail.com>
2025-01-02 10:18:47 +0700

    feat: add styling with tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -11,7 +11,7 @@ function AppLayout() {
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <div>
+      <div className="overflow-y-scroll">
         <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
