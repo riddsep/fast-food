f2f2ef0b63373537ced00dd0727a72f0c079cc00
riddsep <ridoseptiawan00@gmail.com>
2025-01-03 13:15:18 +0700

    minor changes

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex items-center gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <span className="font-semibold">{currentQuantity}</span>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import Deleteitem from "../cart/Deleteitem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newItem = {
@@ -38,11 +43,20 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-
-          {!soldOut && (
-            <Button type="small" onClick={handleAddToCart}>
-              Add to cart
-            </Button>
+          {isInCart ? (
+            <div className="flex items-center gap-3">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <Deleteitem pizzaId={id} />
+            </div>
+          ) : (
+            !soldOut && (
+              <Button type="small" onClick={handleAddToCart}>
+                Add to cart
+              </Button>
+            )
           )}
         </div>
       </div>
